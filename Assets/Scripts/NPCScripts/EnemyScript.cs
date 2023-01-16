d69030c0496c35ad05acee27e1cef3f23faa5b14
Ontovir <120922587+Ontovir@users.noreply.github.com>
2023-01-16 21:32:10 +0400

    16.01.2023 последняя
    Последняя версия

--- Assets/Scripts/NPCScripts/EnemyScript.cs
+++ Assets/Scripts/NPCScripts/EnemyScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class EnemyScript : NonPlayerCharacter
 {
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class EnemyScript : NonPlayerCharacter
     // Update is called once per frame
     void Update()
     {
-        GetMove();
+
     }
 }
--- Assets/Scripts/NPCScripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NPCScripts/NonPlayerCharacter.cs
@@ -5,74 +5,38 @@ using UnityEngine;
 public abstract class NonPlayerCharacter : MonoBehaviour
 {
     [SerializeField] private float healthPoints = 100f;
+    [SerializeField] GameObject spawningPoint;
 
     [Header("NPC Movement Speed Parameters")]
     [SerializeField] private float npcMoveSpeed = 5f;
-    //[SerializeField] private float npcRandomCoordMin = 1f;
-    //[SerializeField] private float npcRandomCoordMax = 3f;
 
     private float xStartPosition;
     private float yStartPosition;
-    public GameObject npcGameObject;
-    Rigidbody2D npcRigidbody2DComponent;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    private float xRandom;
+    private float yRandom;
+    public bool autoMove = true;
     public void StartCoordinates()
     {
-        npcRigidbody2DComponent = GetComponent<Rigidbody2D>();
-        xStartPosition = GetXSpawnPoint();
-        yStartPosition = GetYSpawnPoint();
+        xStartPosition = spawningPoint.transform.position.x;
+        yStartPosition = spawningPoint.transform.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void GetMove()
     {
-        //if (!IsEnemyOutOfRange(xStartPosition, yStartPosition))
+        StartCoroutine(GetRandomMovementPoint());
+        float moveSpeed = npcMoveSpeed * Time.deltaTime;
+        Debug.Log(xRandom);
+        Debug.Log(yRandom);
+        Vector2 moveVector = new Vector2(xStartPosition + xRandom, yStartPosition + yRandom);
+        transform.position = Vector2.MoveTowards(transform.position, moveVector, moveSpeed);
+    }
+    IEnumerator GetRandomMovementPoint()
+    {
+        while (autoMove)
         {
-            float xMovePlus = Random.Range(-9f, 9f);
-            float yMovePlus = Random.Range(-9f, 9f);
-            Vector2 moveVector = new Vector2(xMovePlus, yMovePlus);
-
-            npcRigidbody2DComponent.transform.position = Vector2.MoveTowards
-                (transform.position, moveVector, npcMoveSpeed * Time.deltaTime);
+            xRandom = Random.Range(-20, 20);
+            yRandom = Random.Range(-20, 20);
+            yield return new WaitForSeconds(5f);
         }
-        //else
-        //{
-          //  Vector2 startVector = new Vector2(xStartPosition, yStartPosition);
-            //npcRigidbody2DComponent.transform.position = Vector2.MoveTowards
-              //  (transform.position, startVector, npcMoveSpeed * Time.deltaTime);
-        //}
-    }
-
-    private bool IsEnemyOutOfRange(float xStart, float yStart)
-    {
-        float enemyXPosNow = npcGameObject.transform.position.x;
-        float enemyYPosNow = npcGameObject.transform.position.y; 
-        float xStartPos = xStart;
-        float yStartPos = yStart;
-        if ((enemyXPosNow + enemyYPosNow) >= (xStartPos + yStartPos))
-        {
-            return true;
-        }
-        else return false;
-    }
-
-    private float GetXSpawnPoint()
-    {
-        float xPos = GetComponent<Transform>().position.x;
-        return xPos;
-    }
-    private float GetYSpawnPoint()
-    {
-        float yPos = GetComponent<Transform>().position.y;
-        return yPos;
     }
 }
