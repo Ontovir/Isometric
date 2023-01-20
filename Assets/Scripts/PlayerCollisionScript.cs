be5fd1bdeda9083b382404c55b659ec73ac62c79
Ontovir <120922587+Ontovir@users.noreply.github.com>
2023-01-20 18:24:21 +0400

    Collision настроен
    Коллижн настроен, сообщения появляются. Всё работает хорошо.

--- Assets/Scripts/PlayerCollisionScript.cs
+++ Assets/Scripts/PlayerCollisionScript.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerCollisionScript : MonoBehaviour
 {
-    
+    [SerializeField] TextMeshProUGUI text;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Enemy")
         {
-            Debug.Log("Collision happened");
-
+            CollisionText();
+            StartCoroutine(TextClear());
         }
+    }
+
+    private void CollisionText()
+    {
+        Debug.Log("Collision happened");
+        text.text = "Collision happened!";
+    }
+
+    IEnumerator TextClear()
+    {
+        yield return new WaitForSeconds(2f);
+        text.text = "";
+        StopCoroutine(TextClear());
     }
 }
